6f8fdf0223e91ca6b4ef093dd7275523012a1e39
Leobuaa <pengzimaobuaa@163.com>
2023-11-07 21:15:00 +0800

    feat:添加自定义配置

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -42,11 +42,44 @@ function onLogout (user: Contact) {
 
 async function onMessage (msg: Message) {
   log.info('StarterBot', msg.toString())
+  const delayTime = Math.floor(Math.random() * 50 + 100)
+  log.info('Delay mins: ', delayTime.toString())
 
-  if (msg.text() === 'ding') {
-    await msg.say('dong')
-  }
+  await delay(1000 * 60 * delayTime);
+  await msg.say(getRandomQuestion(questions))
 }
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+function getRandomQuestion(questions: string[]): string {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    return questions[randomIndex];
+}
+
+const questions: string[] = [
+    "最喜欢的电影是什么？",
+    "如果能去任何地方旅行，你会选择哪里？",
+    "你的梦想工作是什么？",
+    "有没有一本书改变了你的看法？",
+    "你最喜欢的季节是哪个？为什么？",
+    "你认为自己最大的成就是什么？",
+    "最喜欢的童年记忆是什么？",
+    "你喜欢哪种音乐类型？",
+    "有没有一项技能你希望能够掌握？",
+    "你最尊敬的人是谁？",
+    "最近看过的一部影响你的电影是哪一部？",
+    "有没有一个习惯你希望能改变？",
+    "你最害怕什么？",
+    "你平常如何放松？",
+    "如果你能拥有一种超能力，那会是什么？",
+    "你认为友谊最重要的是什么？",
+    "如果你能回到过去，你想去哪个时代？",
+    "你最喜欢的节日是什么？",
+    "做过的最疯狂的事是什么？",
+    "如果今天是你的最后一天，你最想做什么？"
+];
 
 const bot = WechatyBuilder.build({
   name: 'ding-dong-bot',
